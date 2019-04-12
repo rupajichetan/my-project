b3d16ad30616db9a6989fc4e3bb58c8649b97d73
rupajichetan <r34chetan@gmail.com>
2019-04-12 18:47:49 -0400

    second commit

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HelloComponent implements OnInit {
 
-  names = ['Prashanth', 'sam'];
+  names = ['Prashanth', 'sam', 'DJ'];
   constructor() { }
 
   ngOnInit() {
